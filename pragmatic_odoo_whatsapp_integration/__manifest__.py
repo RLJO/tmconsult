dfcd8aac4caf55b6f1a67af055a1931be93bac9c
Luis Malave <lmalave@eduwebgroup.com>
2021-07-02 17:42:50 +0200

    [UPD] pragmatic_odoo_whatsapp_integration

--- pragmatic_odoo_whatsapp_integration/__manifest__.py
+++ pragmatic_odoo_whatsapp_integration/__manifest__.py
@@ -1,10 +1,10 @@
 {
     'name': 'Whatsapp Odoo All In One Integration',
-    'version': '14.0.13',
+    'version': '14.0.13.1',
     'category': 'Connector',
     'author': 'Pragmatic TechSoft Pvt Ltd.',
     'website': 'pragtech.co.in',
-    'summary': 'whatsapp connector whatsapp integration odoo Whatsapp crm Whatsapp lead Whatsapp task Whatsapp sale order Whatsapp purchase order Whatsapp invoice Whatsapp payment reminder Whatsapp pos Whatsapp so Whatsapp point of sale whats app communication',
+    'summary': 'whatsapp connector whatsapp integration odoo  Whatsapp task Whatsapp sale order Whatsapp purchase order Whatsapp invoice Whatsapp payment reminder Whatsapp pos Whatsapp so Whatsapp point of sale whats app communication',
     'description': """
 Whatsapp Odoo All In One Integration
 ====================================
@@ -12,7 +12,7 @@ Whatsapp is an immensely popular chatting app used by 1.5 Billion people worldwi
 It has an easy interface and can be used powerfully with Odoo.
 Pragmatic has developed an Odoo app which allows users to use the Whatsapp Application to send messages via Odoo.
 We can send messages from Contacts, Sales, Accounts invoice, Accounts Payments, Credit Notes, Delivery orders, 
-Point of sale, Purchase orders, Project Task, CRM Lead, Payment Reminder, User signup page via the same application. 
+Point of sale, Purchase orders, Payment Reminder, User signup page via the same application. 
 Let us have a look at how this works inside Odoo.
 
 Features of Whatsapp Odoo All in one Integration
@@ -24,16 +24,13 @@ Features of Whatsapp Odoo All in one Integration
         * Add to chatter
         * Add order product information in message such as order amount.
         * Add product details in message such as name and other details:
-    * In CRM, when lead or opportunity will be created then a message will be sent to the salesperson.
-    * In Project Management when when task is created then a WhatsApp message will be sent to the assigned user.
-    * If user sends a reply to task message as done then in odoo project task state is changed to done
     * Send Payment reminder message to customer.
     * Send messages to single or multiple Contacts within Odoo along with multiple attachments in different formats such as doc, pdf, image, audio, video
     * In the Point of sale Odoo app when an order is confirmed, send order details message to customer
     * Send message when a user signs up on the Odoo website page.
 
     """,
-    'depends': ['contacts', 'base_setup', 'delivery', 'sale', 'crm', 'project', 'stock', 'sale_management', 'account', 'purchase_stock'],
+    'depends': ['contacts', 'base_setup', 'delivery', 'sale', 'stock', 'sale_management', 'account', 'purchase_stock'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/send_wp_msg_views.xml',
@@ -46,15 +43,12 @@ Features of Whatsapp Odoo All in one Integration
         'views/stock_picking_views.xml',
         'views/purchase_order_view.xml',
         'views/whatsapp_scheduler.xml',
-        'views/project_task_view.xml',
         'security/security.xml',
         'security/sale_security.xml',
         'views/sale_res_config_settings_view.xml',
         'views/purchase_order_res_config_settings_view.xml',
         'views/stock_picking_res_config_settings_view.xml',
         'views/account_invoicing_res_config_settings_view.xml',
-        'views/crm_res_config_settings_view.xml',
-        'views/project_task_res_config_settings_view.xml',
         'views/account_payment_view.xml',
         'views/login_template.xml',
         'views/whatsapp_message_view.xml'
