360ea23e38bfff13b18c763c89236e380cd68151
rainierking <rainierking@gitlab.com>
2021-09-30 20:46:40 +0800

    [REM] pos_pr

--- pos_pr/static/src/js/pos_button_widgets.js
+++
@@ -1,7 +0,0 @@
-odoo.define('pos_pr.buttons', function (require) {
-    'use strict';
-
-    const PosComponent = require('point_of_sale.PosComponent');
-    const Registries = require('point_of_sale.Registries');
-
-});
