8565e674e0cc4013d10578e70e1be0b49f70737d
bmu-odoo <70546299+bmu-odoo@users.noreply.github.com>
2021-10-04 14:50:27 -0700

    Update __manifest__.py

--- pos_pr/__manifest__.py
+++ pos_pr/__manifest__.py
@@ -20,7 +20,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base',
                 'account',
-                'eduweb_js_utils',
+                # 'eduweb_js_utils',
                 'point_of_sale'],
 
     'data': [
